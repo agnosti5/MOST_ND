51874ee10bcff272f63c4d885e899f8258d607a6
Jim Kelley <kelljim0414@yahoo.com>
2013-11-29 14:54:41 -0500

    Dist folder was missing

--- src/edu/rutgers/MOST/data/ReactionEquationUpdater.java
+++ src/edu/rutgers/MOST/data/ReactionEquationUpdater.java
@@ -5,32 +5,37 @@ import edu.rutgers.MOST.config.LocalConfig;
 public class ReactionEquationUpdater {
 
 	public void updateReactionEquations(int reactionId, SBMLReactionEquation oldEquation, SBMLReactionEquation newEquation) {
-		// update for old equation
-		for (int i = 0; i < oldEquation.getReactants().size(); i++){
-			String reactant = oldEquation.getReactants().get(i).getMetaboliteAbbreviation();
-			LocalConfig.getInstance().getMetaboliteNameIdMap().remove(reactant);
-			updateMetaboliteUsedMap(reactant, "old");
+		if (oldEquation != null) {
+			// update for old equation
+			for (int i = 0; i < oldEquation.getReactants().size(); i++){
+				String reactant = oldEquation.getReactants().get(i).getMetaboliteAbbreviation();
+				LocalConfig.getInstance().getMetaboliteNameIdMap().remove(reactant);
+				updateMetaboliteUsedMap(reactant, "old");
+			}
+			for (int i = 0; i < oldEquation.getProducts().size(); i++){
+				String product = oldEquation.getProducts().get(i).getMetaboliteAbbreviation();
+				LocalConfig.getInstance().getMetaboliteNameIdMap().remove(product);
+				updateMetaboliteUsedMap(product, "old");
+			}
 		}
-		for (int i = 0; i < oldEquation.getProducts().size(); i++){
-			String product = oldEquation.getProducts().get(i).getMetaboliteAbbreviation();
-			LocalConfig.getInstance().getMetaboliteNameIdMap().remove(product);
-			updateMetaboliteUsedMap(product, "old");
-		}
-		// update for new equation
-		for (int i = 0; i < newEquation.getReactants().size(); i++){
-			int metabId = oldEquation.getReactants().get(i).getMetaboliteId();
-			String reactant = oldEquation.getReactants().get(i).getMetaboliteAbbreviation();
-			LocalConfig.getInstance().getMetaboliteNameIdMap().put(reactant, metabId);
-			updateMetaboliteUsedMap(reactant, "new");
-		}
-		for (int i = 0; i < newEquation.getProducts().size(); i++){
-			int metabId = oldEquation.getProducts().get(i).getMetaboliteId();
-			String product = oldEquation.getProducts().get(i).getMetaboliteAbbreviation();
-			LocalConfig.getInstance().getMetaboliteNameIdMap().put(product, metabId);
-			updateMetaboliteUsedMap(product, "new");
-		}
-		LocalConfig.getInstance().getReactionEquationMap().put(reactionId, newEquation);
+		if (newEquation != null) {
+			// update for new equation
+			for (int i = 0; i < newEquation.getReactants().size(); i++){
+				int metabId = oldEquation.getReactants().get(i).getMetaboliteId();
+				String reactant = oldEquation.getReactants().get(i).getMetaboliteAbbreviation();
+				LocalConfig.getInstance().getMetaboliteNameIdMap().put(reactant, metabId);
+				updateMetaboliteUsedMap(reactant, "new");
+			}
+			for (int i = 0; i < newEquation.getProducts().size(); i++){
+				int metabId = oldEquation.getProducts().get(i).getMetaboliteId();
+				String product = oldEquation.getProducts().get(i).getMetaboliteAbbreviation();
+				LocalConfig.getInstance().getMetaboliteNameIdMap().put(product, metabId);
+				updateMetaboliteUsedMap(product, "new");
+			}
+			updateReactionEquationMap(reactionId, newEquation);
+		}		
 	}
+	
 	public void updateMetaboliteUsedMap(String species, String type) {
 		if (type.equals("old")) {
 			int usedCount = (Integer) LocalConfig.getInstance().getMetaboliteUsedMap().get(species);
@@ -47,6 +52,10 @@ public class ReactionEquationUpdater {
 				LocalConfig.getInstance().getMetaboliteUsedMap().put(species, new Integer(1));
 			}
 		}
+	}
+	
+	public void updateReactionEquationMap(int reactionId, SBMLReactionEquation newEquation) {
+		LocalConfig.getInstance().getReactionEquationMap().put(reactionId, newEquation);
 	}
 
 }
