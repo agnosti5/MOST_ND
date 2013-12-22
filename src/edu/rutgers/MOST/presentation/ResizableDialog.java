cbd6a36a48e3053d8ae73d9f606a098879420357
Jim Kelley <kelljim0414@yahoo.com>
2013-12-21 19:13:46 -0500

    Added context menu, needed to change to...
    JTextArea since TextArea has an attached context menu. Also added
    scrollPane.

--- src/edu/rutgers/MOST/presentation/ResizableDialog.java
+++ src/edu/rutgers/MOST/presentation/ResizableDialog.java
@@ -1,15 +1,25 @@
 package edu.rutgers.MOST.presentation;
 
 import javax.swing.*;  
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 import java.awt.*;  
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
    
 // based on code from http://www.coderanch.com/t/344723/GUI/java/JDialog-resize
-public class ResizableDialog extends javax.swing.JDialog  
+public class ResizableDialog extends JDialog  
 {  
     /**
 	 * 
@@ -22,6 +32,9 @@ public class ResizableDialog extends javax.swing.JDialog
     private JPanel MessagePanel;  
     private JLabel Label;
     public boolean messageShown;
+    
+    private final JMenuItem copyItem = new JMenuItem("Copy");
+	private final JMenuItem selectAllItem = new JMenuItem("Select All");
     
     private String errorTitle;
 
@@ -64,15 +77,15 @@ public class ResizableDialog extends javax.swing.JDialog
     {  
     	setErrorTitle("Error");
     	setErrorDescription("Testing a long, long, long, long error message");
-    	setErrorMessage("Testing\nTesting");
+    	setErrorMessage("Testing\nTesting\nTesting\nTesting");
     	
     	messageShown = false;
-    	LabelPanel = new javax.swing.JPanel(); 
-    	Label = new javax.swing.JLabel(); 
-        ButtonPanel = new javax.swing.JPanel();  
-        OKButton = new javax.swing.JButton();  
-        DetailsButton = new javax.swing.JButton();  
-        MessagePanel = new javax.swing.JPanel();       
+    	LabelPanel = new JPanel(); 
+    	Label = new JLabel(); 
+        ButtonPanel = new JPanel();  
+        OKButton = new JButton();  
+        DetailsButton = new JButton();  
+        MessagePanel = new JPanel();   
               
         getRootPane().setDefaultButton(OKButton);
         
@@ -80,7 +93,7 @@ public class ResizableDialog extends javax.swing.JDialog
         Label.setText(getErrorDescription());
    
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);  
-   
+        
         LabelPanel.add(Label);
         getContentPane().add(LabelPanel, java.awt.BorderLayout.NORTH); 
         getContentPane().add(MessagePanel, java.awt.BorderLayout.SOUTH);  
@@ -100,26 +113,35 @@ public class ResizableDialog extends javax.swing.JDialog
         
         DetailsButton.setText("Details >>"); 
         DetailsButton.setMnemonic(KeyEvent.VK_D);
-        DetailsButton.addActionListener(new java.awt.event.ActionListener()  
+        DetailsButton.addActionListener(new ActionListener()  
         {  
-            public void actionPerformed(java.awt.event.ActionEvent evt)  
+            public void actionPerformed(ActionEvent evt)  
             {  
             	// Add or remove some stuff!  
                 if(!messageShown) {
-                	TextArea textArea = new TextArea();
-                	MessagePanel.add(textArea);
+                	JTextArea textArea = createTextArea();
+                	//JTextArea textArea = new JTextArea();
                 	DetailsButton.setText("<< Details");
                 	textArea.setText(getErrorMessage());
+                	//add reaction field to scroll pane		
+            		JScrollPane scrollPane = new JScrollPane(textArea);
+            		scrollPane.setBorder(BorderFactory.createEmptyBorder(10,10,10,20));
+            		scrollPane.setPreferredSize(new Dimension(500, 200));
+                	MessagePanel.add(scrollPane);
                 	messageShown = true;
                 } else {
                 	DetailsButton.setText("Details >>");
                 	Component[] old = MessagePanel.getComponents();  
                     for( Component c : old )  
                     {   
-                        if( c instanceof TextArea )  
+                        if( c instanceof JTextArea )  
                         {  
                         	MessagePanel.remove(c);  
                         } 
+                        if( c instanceof JScrollPane )  
+                        {  
+                        	MessagePanel.remove(c);  
+                        }
                     } 
                     messageShown = false;
                 }
@@ -136,6 +158,108 @@ public class ResizableDialog extends javax.swing.JDialog
         pack();  
     }                                                                                                  
     
+    public JTextArea createTextArea() {
+    	final JTextArea textArea = new JTextArea();
+
+    	textArea.setSize(200, 200);
+    	textArea.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
+    	//textArea.setFont(new Font("monospaced", Font.PLAIN, 14));
+    	textArea.setEditable(false);
+
+    	final JPopupMenu popupMenu = new JPopupMenu(); 
+		popupMenu.add(copyItem);
+		copyItem.setEnabled(true);
+		copyItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) { 	
+				setClipboardContents(textArea.getSelectedText());							
+			}
+		});
+		popupMenu.add(selectAllItem);
+		selectAllItem.setEnabled(true);
+		selectAllItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) { 
+				textArea.selectAll();							
+			}
+		});
+		
+		textArea.addMouseListener(new MouseAdapter() {
+
+			public void mousePressed(MouseEvent e)  {check(e);}
+			public void mouseReleased(MouseEvent e) {check(e);}
+
+			public void check(MouseEvent e) {
+				if (e.isPopupTrigger()) { //if the event shows the menu
+					popupMenu.show(textArea, e.getX(), e.getY()); 
+				}
+			}
+		});	
+		
+		textArea.getDocument().addDocumentListener(new DocumentListener() {
+			public void changedUpdate(DocumentEvent e) {
+				fieldChangeAction();
+			}
+			public void removeUpdate(DocumentEvent e) {
+				fieldChangeAction();
+			}
+			public void insertUpdate(DocumentEvent e) {
+				fieldChangeAction();
+			}
+			public void fieldChangeAction() {
+				if (textArea.getText().length() > 0) {
+					copyItem.setEnabled(true);
+					selectAllItem.setEnabled(true);
+				} else {
+					copyItem.setEnabled(false);
+					selectAllItem.setEnabled(false);
+				}
+			}
+		});
+		
+		return textArea;
+    	
+    }
+    
+  //from http://www.javakb.com/Uwe/Forum.aspx/java-programmer/21291/popupmenu-for-a-cell-in-a-JXTable
+  	private static String getClipboardContents(Object requestor) {
+  		Transferable t = Toolkit.getDefaultToolkit()
+  		.getSystemClipboard().getContents(requestor);
+  		if (t != null) {
+  			DataFlavor df = DataFlavor.stringFlavor;
+  			if (df != null) {
+  				try {
+  					Reader r = df.getReaderForText(t);
+  					char[] charBuf = new char[512];
+  					StringBuffer buf = new StringBuffer();
+  					int n;
+  					while ((n = r.read(charBuf, 0, charBuf.length)) > 0) {
+  						buf.append(charBuf, 0, n);
+  					}
+  					r.close();
+  					return (buf.toString());
+  				} catch (IOException ex) {
+  					ex.printStackTrace();
+  				} catch (UnsupportedFlavorException ex) {
+  					ex.printStackTrace();
+  				}
+  			}
+  		}
+  		return null;
+  	}
+
+  	private static boolean isClipboardContainingText(Object requestor) {
+  		Transferable t = Toolkit.getDefaultToolkit()
+  		.getSystemClipboard().getContents(requestor);
+  		return t != null
+  		&& (t.isDataFlavorSupported(DataFlavor.stringFlavor) || t
+  				.isDataFlavorSupported(DataFlavor.plainTextFlavor));
+  	}
+  	
+  	private static void setClipboardContents(String s) {
+  	      StringSelection selection = new StringSelection(s);
+  	      Toolkit.getDefaultToolkit().getSystemClipboard().setContents(
+  	            selection, selection);
+  	}
+
     public static void main(String[] args) {
     	final ArrayList<Image> icons = new ArrayList<Image>(); 
 		icons.add(new ImageIcon("etc/most16.jpg").getImage()); 
